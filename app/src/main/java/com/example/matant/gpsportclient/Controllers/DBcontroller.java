8150f542bde90323de0716d23eae44c9856ca7f0
matant <matant86@gmail.com>
2015-08-09 20:44:03 +0300

    DB controller implemintation

--- app/src/main/java/com/example/matant/gpsportclient/Controllers/DBcontroller.java
+++ app/src/main/java/com/example/matant/gpsportclient/Controllers/DBcontroller.java
@@ -2,18 +2,12 @@ package com.example.matant.gpsportclient.Controllers;
 
 import android.os.AsyncTask;
 
-import org.apache.http.NameValuePair;
-
-import java.util.List;
-
 /**
  * Created by matant on 8/9/2015.
  */
-public class DBcontroller extends AsyncTask<List<NameValuePair>,Void,String> {
-
-//shit happens
+public class DBcontroller extends AsyncTask {
     @Override
-    protected String doInBackground(List<NameValuePair>... params) {
+    protected Object doInBackground(Object[] params) {
         return null;
     }
 }
