617150d3207e507f531148f9fe3cd936fb1f04d0
matant <matant86@gmail.com>
2015-08-09 20:15:24 +0300

    DB controller implemintation

---
+++ app/src/main/java/com/example/matant/gpsportclient/Controllers/DBcontroller.java
@@ -0,0 +1,19 @@
+package com.example.matant.gpsportclient.Controllers;
+
+import android.os.AsyncTask;
+
+import org.apache.http.NameValuePair;
+
+import java.util.List;
+
+/**
+ * Created by matant on 8/9/2015.
+ */
+public class DBcontroller extends AsyncTask<List<NameValuePair>,Void,String> {
+
+
+    @Override
+    protected String doInBackground(List<NameValuePair>... params) {
+        return null;
+    }
+}
