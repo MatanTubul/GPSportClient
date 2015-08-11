7e3d274aeca4b81f6742af68571de9e2e2d51277
matant <matant86@gmail.com>
2015-08-11 23:12:59 +0300

    DBcontroller rename-not working good-fixed

--- app/src/main/java/com/example/matant/gpsportclient/Controllers/Login.java
+++ app/src/main/java/com/example/matant/gpsportclient/Controllers/Login.java
@@ -28,7 +28,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener,Asy
 
     EditText userNameEditText, passwordEditText;
     Button loginB, signUpB;
-    DBController dbController;
+    DBcontroller dbController;
     TextView forgotPasswordTV;
     boolean userCanLogIn;
 
@@ -36,7 +36,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener,Asy
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        dbController =   new DBController();
+        dbController =   new DBcontroller();
         dbController.delegate = this;
         userCanLogIn = false;
 
