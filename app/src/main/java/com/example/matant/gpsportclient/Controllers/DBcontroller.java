66dcbea08cf1915f8de1a0aef38db14a2de45f6e
matant <matant86@gmail.com>
2015-08-11 15:49:55 +0300

    update DBcontroller by nir from github

--- app/src/main/java/com/example/matant/gpsportclient/Controllers/DBcontroller.java
+++ app/src/main/java/com/example/matant/gpsportclient/Controllers/DBcontroller.java
@@ -1,10 +1,11 @@
 package com.example.matant.gpsportclient.Controllers;
 
-import android.content.Context;
+import android.content.ContentValues;
 import android.os.AsyncTask;
-import android.util.Log;
-import android.view.View;
 
+import com.example.matant.gpsportclient.AsyncResponse;
+
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -12,90 +13,54 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by matant on 8/9/2015.
+ * Created by Nir b on 11/08/2015.
  */
-public  class DBcontroller extends AsyncTask<List<NameValuePair>,Void,String> {
+public class DBController extends AsyncTask <List<NameValuePair>, Void, InputStream>{
 
-    public DBcontroller() {
+    private final String url= "http://10.0.2.2/test.php";
+    public AsyncResponse delegate= null;
+    private InputStream is = null;
+
+    @Override
+    protected InputStream doInBackground(List<NameValuePair>... params) {
+        return postDataToServer(params[0]);
     }
 
     @Override
-    protected String doInBackground(List<NameValuePair>... params) {
-        String res = "";
-        Log.d("forgot password","begin recover password");
-        HttpClient httpClient = new DefaultHttpClient();
-        HttpPost httpPost = new HttpPost("http://10.0.2.2/gpSportserver/forgot_password.php");
-
-        try {
-            UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(params[0]);
-            httpPost.setEntity(urlEncodedFormEntity);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            // HttpResponse is an interface just like HttpPost.
-            //Therefore we can't initialize them
-            Log.d("httpPost","execute post");
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-
-            // According to the JAVA API, InputStream constructor do nothing.
-            //So we can't initialize InputStream although it is not an interface
-            InputStream inputStream = httpResponse.getEntity().getContent();
-
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            Log.d("InputStreamReader","execute inputStreamReader");
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            StringBuilder stringBuilder = new StringBuilder();
-            Log.d("StringBuilder","execute StringBuilder");
-            String bufferedStrChunk = null;
-
-            while ((bufferedStrChunk = bufferedReader.readLine()) != null) {
-                stringBuilder.append(bufferedStrChunk+"\n");
-            }
-            Log.d("inputStreamReader","execute close");
-            inputStreamReader.close();
-            res = stringBuilder.toString();
-            Log.d("json result",res);
-            JSONObject jarray = new JSONObject(res);
-
-            Log.d("json result",jarray.toString());
-            try {
-                Log.d("JSON Msg:", jarray.getString("msg"));
-            } catch (JSONException e) {
-                Log.d("JSon Failed" , jarray.toString());
-                e.printStackTrace();
-            }
-            return res;
-
-        } catch (ClientProtocolException cpe) {
-            System.out.println("Firstption caz of HttpResponese :" + cpe);
-            cpe.printStackTrace();
-        } catch (IOException ioe) {
-            System.out.println("Secondption caz of HttpResponse :" + ioe);
-            ioe.printStackTrace();
-        } catch (JSONException e) {
-            Log.d("JSON error:", "failed to create jarray");
-            e.printStackTrace();
-        }
-        return null;
+    protected void onPostExecute(InputStream is) {
+        delegate.handleResponse(is);
     }
 
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-        Log.d("Answer from server", s);
+    private InputStream postDataToServer(List<NameValuePair> list) {
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(url);
+        try {
+            post.setEntity(new UrlEncodedFormEntity(list));
+            HttpResponse response = client.execute(post);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+
+        } catch (ClientProtocolException e) {
+            // process execption
+        } catch (IOException e) {
+            // process execption
+        }
+
+        return is;
     }
-}
+
+    }
+
+
+
+
+
+
+
