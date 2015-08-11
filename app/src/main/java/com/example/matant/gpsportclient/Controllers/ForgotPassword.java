2d62003d571643a226b1f08cc279765e085e907a
matant <matant86@gmail.com>
2015-08-11 15:39:50 +0300

    SignUp implementation

--- app/src/main/java/com/example/matant/gpsportclient/Controllers/ForgotPassword.java
+++ app/src/main/java/com/example/matant/gpsportclient/Controllers/ForgotPassword.java
@@ -1,10 +1,8 @@
 package com.example.matant.gpsportclient.Controllers;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
