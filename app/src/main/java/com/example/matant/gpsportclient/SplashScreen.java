7387a7e71a0b884d30015bf0b424c2586acae013
matant <matant86@gmail.com>
2015-08-27 14:15:48 +0300

    Splash Screen- internet check was added before loading the application

--- app/src/main/java/com/example/matant/gpsportclient/SplashScreen.java
+++ app/src/main/java/com/example/matant/gpsportclient/SplashScreen.java
@@ -1,9 +1,15 @@
 package com.example.matant.gpsportclient;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
 
 import com.example.matant.gpsportclient.Controllers.Login;
@@ -18,24 +24,53 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
+        if (!isNetworkAvailable()) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Connection Error")
+                    .setMessage("Internet seems to be broken")
+                    .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                              int pid = android.os.Process.myPid();
+                              android.os.Process.killProcess(pid);
+                              System.exit(0);
+                        }
+                    })
+                    .setIconAttribute(android.R.attr.alertDialogIcon)
+                    .show();
+
+        }else {
+
+            new Handler().postDelayed(new Runnable() {
  
             /*
              * Showing splash screen with a timer. This will be useful when you
              * want to show case your app logo / company
              */
 
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, Login.class);
-                startActivity(i);
+                @Override
+                public void run() {
+                    // This method will be executed once the timer is over
+                    // Start your app main activity
+                    Intent i = new Intent(SplashScreen.this, Login.class);
+                    startActivity(i);
 
-                // close this activity
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+                    // close this activity
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
+    }
+
+    /**
+     * function that check if the internet services is available in case not application will be terminated
+     * @return false-in case internet is not available
+     */
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected =  activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+        return  isConnected;
     }
 
 }
