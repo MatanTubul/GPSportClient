8cfd6a4aa145f68d9c983f004a6934e0e24879dd
matant <matant86@gmail.com>
2015-12-19 14:39:07 +0200

    commit -19/12, temporary LocationServices class

---
+++ app/src/main/java/com/example/matant/gpsportclient/InterfacesAndConstants/OnLocationFoundListener.java
@@ -0,0 +1,11 @@
+package com.example.matant.gpsportclient.InterfacesAndConstants;
+
+import android.location.Location;
+
+/**
+ * Created by matant on 12/17/2015.
+ */
+public interface OnLocationFoundListener {
+    public void onLocationFound(Location loc);
+    public void getLocationInProccess();
+}
